2832e11d111364c6d82ae198ab0ba7b1bcfa27a8
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-03-01 10:14:28 -0600

    Add functions to resolve if a path is a file or a directory

--- prime_vx/shell/shell.py
+++ prime_vx/shell/shell.py
@@ -1,4 +1,4 @@
-from os.path import abspath, expanduser
+from os.path import abspath, expanduser, isdir, isfile
 from pathlib import Path
 from subprocess import PIPE, CompletedProcess, run
 
@@ -14,3 +14,11 @@ def runProgram(cmd: str) -> CompletedProcess[bytes]:
 
 def resolvePath(path: Path) -> Path:
     return Path(abspath(path=expanduser(path=path)))
+
+
+def isFile(path: Path) -> bool:
+    return isfile(path=resolvePath(path=path))
+
+
+def isDirectory(path: Path) -> bool:
+    return isdir(s=resolvePath(path=path))
