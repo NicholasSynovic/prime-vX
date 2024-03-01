c1a3b700c30a9d94c6ea53668df9abafb869d316
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-03-01 15:17:59 -0600

    Migrate code into class and write documentation

--- prime_vx/db/_classes/_dbHandler.py
+++ prime_vx/db/_classes/_dbHandler.py
@@ -7,19 +7,50 @@ from sqlalchemy.engine import Engine
 
 
 class DBHandler_Protocol(Protocol):
+    """
+    DBHandler_Protocol
+
+    Top level Protocol (a class for linters to ensure that variables andmethods are defined) for database handlers to enforce common variablesare defined in all implementations
+    """
+
     path: Path
     exists: bool
 
 
 class SQLiteHandler_Protocol(DBHandler_Protocol, Protocol):
+    """
+    SQLiteHandler_Protocol
+
+    SQLite3 handler specific Protcol to enforce common variables are defined in all implementations
+    """
+
     engine: Engine
 
 
 class SQLiteHandler_ABC(SQLiteHandler_Protocol, metaclass=ABCMeta):
-    @abstractmethod
-    def write(self, df: DataFrame, tableName: str, engine: Engine) -> None:
-        ...
+    """
+    SQLiteHandler_ABC
+
+    SQLite3 handler abstract base class (ABC) (i.e., interface) to enforce common methods are defined in all implementations. Inherits the SQLiteHandler_Protocol Protocol to enforce variables
+    """
 
     @abstractmethod
     def createMetadata(self) -> None:
+        """
+        createMetadata
+
+        Create table columns and metadata relevant to the specific SQLite3 handler and ensure that the SQLAlchemy engine is aware of them.
+        """
+        ...
+
+    @abstractmethod
+    def write(self, df: DataFrame) -> None:
+        """
+        write
+
+        Write DataFrame rows to database. Ignore rows where the primary key is already stored in the database.
+
+        :param df: A DataFrame of information to store in the database. DataFrame column names must be the same as database column names
+        :type df: DataFrame
+        """
         ...
--- prime_vx/db/sqlite.py
+++ prime_vx/db/sqlite.py
@@ -1,16 +1,15 @@
 from pathlib import Path
-from typing import List
+from typing import Any, List
 
 from pandas import DataFrame
 from progress.bar import Bar
-from sqlalchemy import create_engine
-from sqlalchemy.engine import Engine
-from sqlalchemy.engine.base import Engine
+from sqlalchemy import Column, MetaData, String, Table, create_engine
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.sql.base import ReadOnlyColumnCollection
 
 from prime_vx.db._classes._dbHandler import SQLiteHandler_ABC
-from prime_vx.db._schemas._sqliteSchemas import vcsMetadataSchema
 from prime_vx.shell.shell import isDirectory, isFile, resolvePath
+from prime_vx.vcs import VCS_METADATA_KEYS
 
 
 class VCS_DB(SQLiteHandler_ABC):
@@ -35,7 +34,35 @@ class VCS_DB(SQLiteHandler_ABC):
         self.engine = create_engine(url=f"sqlite:///{self.path}")
 
     def createMetadata(self) -> None:
-        vcsMetadataSchema(engine=self.engine, tableName=self.tableName)
+        metadata: MetaData = MetaData()
+
+        table: Table = Table(
+            self.tableName,
+            metadata,
+            Column("commitHash", String, primary_key=True),
+            Column("treeHash", String),
+            Column("parentHashes", String),
+            Column("authorName", String),
+            Column("authorEmail", String),
+            Column("authorDate", String),
+            Column("committerName", String),
+            Column("committerEmail", String),
+            Column("committerDate", String),
+            Column("refName", String),
+            Column("refNameSource", String),
+            Column("gpgSignature", String),
+        )
+
+        columnData: ReadOnlyColumnCollection[str, Column[Any]] = table.columns
+
+        # TODO: Move database table schema validation to data_model module
+        if [x.key for x in columnData] == VCS_METADATA_KEYS:
+            metadata.create_all(bind=self.engine)
+        else:
+            print(
+                "Invalid table schema. Table schema does not align with VCS_METADATA_KEYS"
+            )
+            quit()
 
     def write(self, df: DataFrame) -> None:
         dfPerRow: List[DataFrame] = [DataFrame(data=row).T for _, row in df.iterrows()]
