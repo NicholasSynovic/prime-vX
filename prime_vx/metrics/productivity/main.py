4ed088c6f7a93ba447f8ab1ac83dbfbcb1346c4f
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-03-29 15:09:32 -0500

    Add code for computing productivity

--- prime_vx/metrics/productivity/main.py
+++ prime_vx/metrics/productivity/main.py
@@ -1,49 +1,67 @@
+from datetime import datetime
 from typing import List, Tuple
 
 from pandas import DataFrame, Grouper
 from pandas.core.groupby.generic import DataFrameGroupBy
 from progress.bar import Bar
 
-from prime_vx.datamodels.metrics.loc import LOC_DF_DATAMODEL
 
-DATA_STOR_DICT: dict[str, List] = {
-    "commitHash": [],
-    "startDateBucket": [],
-    "endDateBucket": [],
-    "productivity": [],
-}
+def computeProductivity(groups: DataFrameGroupBy, datum: Tuple[str, str]) -> DataFrame:
+    # TODO: Add doc string
 
+    data: dict[str, List[int | float | datetime]] = {
+        "bucket": [],
+        "bucket_start": [],
+        "bucket_end": [],
+        "effort_LOC": [],
+        "effort_KLOC": [],
+        "productivity_LOC": [],
+        "productivity_KLOC": [],
+    }
 
-def computeProductivity(
-    groups: DataFrameGroupBy, datum: Tuple[str, str, dict]
-) -> DataFrame:
+    bucket: int = 1
+
     with Bar(f"Computing {datum[0]} productivity...", max=len(groups)) as bar:
         group: DataFrame
         for _, group in groups:
-            print(group["delta_loc"].abs().sum())
-            quit()
+            effortLOC: int = group["delta_loc"].abs().sum()
+            effortKLOC: float = group["delta_kloc"].abs().sum()
+            productivityLOC: float = effortLOC / bucket
+            productivityKLOC: float = effortKLOC / bucket
+
+            data["bucket"].append(bucket)
+            data["bucket_start"].append(group["committerDate"].min().to_pydatetime())
+            data["bucket_end"].append(group["committerDate"].max().to_pytdatetime())
+            data["effort_KLOC"].append(effortKLOC)
+            data["effort_LOC"].append(effortLOC)
+            data["productivity_KLOC"].append(productivityKLOC)
+            data["productivity_LOC"].append(productivityLOC)
+
+            bucket += 1
             bar.next()
+
+    return DataFrame(data=data)
 
 
 def main(df: DataFrame) -> DataFrame:
     # TODO: Add docstring
 
-    data: List[Tuple[str, str, dict]] = [
-        ("daily", "D", DATA_STOR_DICT),
-        ("weekly", "W", DATA_STOR_DICT),
-        ("two week", "2W", DATA_STOR_DICT),
-        ("monthly", "ME", DATA_STOR_DICT),
-        ("two month", "2ME", DATA_STOR_DICT),
-        ("three month", "QE", DATA_STOR_DICT),
-        ("six month", "2QE", DATA_STOR_DICT),
-        ("annual", "YE", DATA_STOR_DICT),
+    data: List[Tuple[str, str]] = [
+        ("daily", "D"),
+        ("weekly", "W"),
+        ("two week", "2W"),
+        ("monthly", "ME"),
+        ("two month", "2ME"),
+        ("three month", "QE"),
+        ("six month", "2QE"),
+        ("annual", "YE"),
     ]
 
     relevantDataDF: DataFrame = df[
         ["commitHash", "committerDate", "delta_loc", "delta_kloc"]
     ]
 
-    datum: Tuple[str, str, dict]
+    datum: Tuple[str, str]
     for datum in data:
         frequency: str = datum[1]
 
@@ -54,4 +72,4 @@ def main(df: DataFrame) -> DataFrame:
             )
         )
 
-        tempDF: DataFrame = computeProductivity(groups=groups, datum=datum)
+        df: DataFrame = computeProductivity(groups=groups, datum=datum)
