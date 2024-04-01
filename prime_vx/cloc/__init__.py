229ca80cbc561e751f7e2d596c12be73d531ed55
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-04-01 13:41:31 -0500

    Add a list of counters to support

--- prime_vx/cloc/__init__.py
+++ prime_vx/cloc/__init__.py
@@ -10,3 +10,20 @@ CLOC_KEY_LIST: List[str] = [
     "codeLineCount",
     "json",
 ]
+
+# TODO
+# Add the following tools:
+# pygount (https://pypi.org/project/pygount/)
+# SLOCCount (https://dwheeler.com/sloccount/) + sloc2html (https://dwheeler.com/sloccount/sloc2html.py.txt)
+# cloc (https://github.com/AlDanial/cloc)
+# gocloc (https://github.com/hhatto/gocloc)
+# loc (https://github.com/cgag/loc)
+# loccount (https://gitlab.com/esr/loccount)
+# polygloy (https://github.com/vmchale/polyglot)
+# Tokei (https://github.com/XAMPPRocky/tokei)
+# sloc (https://github.com/flosse/sloc)
+# gloc (https://github.com/JoaoDanielRufino/gcloc)
+# ohcount (https://github.com/blackducksoftware/ohcount/)
+# sclc (https://code.google.com/archive/p/sclc/)
+# Sonar (https://www.sonarsource.com/products/sonarqube/)
+# QSM List (https://www.qsm.com/CodeCounters.html)
