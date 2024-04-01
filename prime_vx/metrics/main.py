786aed2308905551eb0dde31cedb4c7ef01d5ab5
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-04-01 14:27:40 -0500

    Support writing productivity metrics to database

--- prime_vx/metrics/main.py
+++ prime_vx/metrics/main.py
@@ -8,12 +8,7 @@ from pyfs import isFile, resolvePath
 from prime_vx.datamodels.cloc import CLOC_DF_DATAMODEL
 from prime_vx.datamodels.metrics.loc import LOC_DF_DATAMODEL
 from prime_vx.datamodels.vcs import VCS_DF_DATAMODEL
-from prime_vx.db import (
-    CLOC_DB_TABLE_NAME,
-    LOC_DB_TABLE_NAME,
-    PRODUCTIVITY_DB_TABLE_NAME_SUFFIX,
-    VCS_DB_TABLE_NAME,
-)
+from prime_vx.db import CLOC_DB_TABLE_NAME, LOC_DB_TABLE_NAME, VCS_DB_TABLE_NAME
 from prime_vx.db.sqlite import SQLite
 from prime_vx.exceptions import InvalidDBPath, InvalidMetricSubprogram
 from prime_vx.metrics.loc.main import main as locMain
@@ -67,10 +62,12 @@ def main(namespace: Namespace) -> None:
             dfs: dict[str, DataFrame] = prodMain(df=mergedDF)
 
             key: str
-            for key in dfs.keys():
-                print(key)
+            for tableName, df in dfs.items():
+                db.write(
+                    df=df,
+                    tableName=tableName,
+                    includeIndex=False,
+                )
 
-            # df: DataFrame = locMain(df=clocDF)
-            # db.write(df=df, tableName="loc", includeIndex=True)
         case _:
             raise InvalidMetricSubprogram
