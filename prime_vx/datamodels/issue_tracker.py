d77c84d7387d4c09450e43268c5df3d98faf5e5b
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-04-12 09:46:35 -0500

    Update datamodel for storing issue tracking information

--- prime_vx/datamodels/issue_tracker.py
+++ prime_vx/datamodels/issue_tracker.py
@@ -1,4 +1,4 @@
-from typedframe import TypedDataFrame
+from typedframe import DATE_TIME_DTYPE, TypedDataFrame
 
 
 class IT_DF_DATAMODEL(TypedDataFrame):
@@ -6,8 +6,8 @@ class IT_DF_DATAMODEL(TypedDataFrame):
         "id": int,
         "node_id": str,
         "state": str,
-        "date_opened": float,
-        "date_closed": float,
+        "date_opened": DATE_TIME_DTYPE,
+        "date_closed": DATE_TIME_DTYPE,
         "url": str,
         "json": str,
     }
