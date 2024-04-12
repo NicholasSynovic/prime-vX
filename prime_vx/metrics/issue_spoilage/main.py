0e20a64cfa35580cfe06f5ff96e48c7b79646d60
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-04-12 15:19:55 -0500

    Implement method for computing issue spoilage

--- prime_vx/metrics/issue_spoilage/main.py
+++ prime_vx/metrics/issue_spoilage/main.py
@@ -1,64 +1,75 @@
-from typing import List
+from typing import List, Literal
 
-import pandas
-from intervaltree import IntervalTree
-from pandas import DataFrame, Series, Timestamp
+from numpy import datetime64
+from pandas import DataFrame, DatetimeIndex, Timestamp, date_range
 from progress.bar import Bar
 
-TIME_FORMAT_STR: str = "%Y-%m-%d"
+TIME_FORMAT: str = "%Y-%m-%d"
 
 
-def createDateToIntervalMapping(dates: Series) -> dict[Timestamp, int]:
-    sortedDates: List[Timestamp] = [
-        Timestamp(date)
-        for date in dates.dt.strftime(date_format=TIME_FORMAT_STR).unique()
-    ]
+def computeIssueSpoilage(
+    df: DataFrame,
+    freq: Literal[
+        "D",
+        "W",
+        "2W",
+        "ME",
+        "2ME",
+        "QE",
+        "2QE",
+        "YE",
+    ],
+) -> dict[str, List[int | datetime64]]:
+    data: dict[str, List[int | datetime64]] = {
+        "bucket": [],
+        "bucket_start": [],
+        "bucket_end": [],
+        "spoiled_issues": [],
+    }
 
-    data: dict[Timestamp, int] = {date: sortedDates.index(date) for date in sortedDates}
+    bucket: int = 1
 
-    return data
+    minimumDate: Timestamp = df["date_opened"].min()
+    maximumDate: Timestamp = df["date_closed"].max()
 
+    minimumDateFormatted: Timestamp = Timestamp(
+        ts_input=minimumDate.strftime(format=TIME_FORMAT),
+    )
+    maximumDateFormatted: Timestamp = Timestamp(
+        ts_input=maximumDate.strftime(format=TIME_FORMAT),
+    )
 
-def createIntervalTree(dates: DataFrame, map_: dict[Timestamp, int]) -> IntervalTree:
-    it: IntervalTree = IntervalTree()
+    dateRange: DatetimeIndex = date_range(
+        start=minimumDateFormatted,
+        end=maximumDateFormatted,
+        freq=freq,
+    )
 
-    maximumDate: Timestamp = list(map_.keys())[-1]
+    maxIterations: int = len(dateRange)
+    with Bar(
+        f"Counting number of spoiled issues (frequency: {freq})...", max=maxIterations
+    ) as bar:
+        idx: int
+        for idx in range(maxIterations):
+            currentDate: Timestamp = dateRange[idx]
 
-    with Bar("Creating interval tree...", max=dates.shape[0]) as bar:
-        openDate: Timestamp
-        closeDate: Timestamp
-        for openDate, closeDate in dates.itertuples(index=False):
-            openDay: Timestamp = Timestamp(
-                ts_input=openDate.strftime(format=TIME_FORMAT_STR)
-            )
-
-            closeDay: Timestamp
+            nextDate: Timestamp
             try:
-                closeDay = Timestamp(
-                    ts_input=closeDate.strftime(format=TIME_FORMAT_STR)
-                )
-            except ValueError:
-                closeDay = maximumDate
+                nextDate = dateRange[idx + 1]
+            except IndexError:
+                nextDate = Timestamp().max
 
-            startInterval: int = map_[openDay]
-            endInterval: int = map_[closeDay]
+            spoiledIssuesCount: int = df[
+                (df["date_opened"] >= currentDate) & (df["date_closed"] < nextDate)
+            ].shape[0]
 
-            it.addi(begin=startInterval, end=endInterval + 1, data=True)
+            data["bucket"].append(bucket)
+            data["bucket_start"].append(currentDate.to_datetime64())
+            data["bucket_end"].append(nextDate.to_datetime64())
+            data["spoiled_issues"].append(spoiledIssuesCount)
 
-            bar.next()
-
-    return it
+        return data
 
 
 def main(df: DataFrame) -> dict[str, DataFrame]:
-    openDates: Series = df["date_opened"]
-    closedDates: Series = df["date_closed"]
-
-    datesDF: DataFrame = df[["date_opened", "date_closed"]]
-    datesSeries: Series = pandas.concat(objs=[openDates, closedDates], axis=0)
-
-    dateToIntervalMap: dict[Timestamp, int] = createDateToIntervalMapping(
-        dates=datesSeries
-    )
-
-    it: IntervalTree = createIntervalTree(dates=datesDF, map_=dateToIntervalMap)
+    computeIssueSpoilage(df=df, freq="D")
