cb38dce0ce7da734deed2f3f248e899e73205f83
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-04-15 15:07:58 -0500

    Reimplement issue spoilage

--- prime_vx/metrics/issue_spoilage/reimplement.py
+++ prime_vx/metrics/issue_spoilage/reimplement.py
@@ -1,8 +1,21 @@
 from typing import List, Tuple
 
 from intervaltree import IntervalTree
+from numpy import datetime64
 from pandas import DataFrame, Timedelta, Timestamp
 from progress.bar import Bar
+
+from prime_vx.datamodels.metrics.issue_spoilage import ISSUE_SPOILAGE_DF_DATAMODEL
+from prime_vx.db import (
+    ANNUAL_ISSUE_SPOILAGE_DB_TABLE_NAME,
+    DAILY_ISSUE_SPOILAGE_DB_TABLE_NAME,
+    MONTHLY_ISSUE_SPOILAGE_DB_TABLE_NAME,
+    SIX_MONTH_ISSUE_SPOILAGE_DB_TABLE_NAME,
+    THREE_MONTH_ISSUE_SPOILAGE_DB_TABLE_NAME,
+    TWO_MONTH_ISSUE_SPOILAGE_DB_TABLE_NAME,
+    TWO_WEEK_ISSUE_SPOILAGE_DB_TABLE_NAME,
+    WEEKLY_ISSUE_SPOILAGE_DB_TABLE_NAME,
+)
 
 
 def identifyDayZero_N(df: DataFrame) -> Tuple[Timestamp, Timestamp]:
@@ -19,11 +32,66 @@ def buildIntervalTree(df: DataFrame, dayZero: Timestamp) -> IntervalTree:
     end: List[Timedelta] = (df["date_closed"] - dayZero).to_list()
 
     with Bar("Creating interval tree...", max=df.shape[0]) as bar:
-        for i in list(zip(start, end)):
-            it.addi(begin=i[0].days, end=i[1].days + 1, data=1)
+        timedelta: Timedelta
+        for timedelta in list(zip(start, end)):
+            it.addi(
+                begin=timedelta[0].days,
+                end=timedelta[1].days + 1,
+                data=1,
+            )
             bar.next()
 
     return it
+
+
+def computeIssueSpoilage(
+    it: IntervalTree, dayZero: Timestamp, step: int = 1
+) -> DataFrame:
+    data: dict[str, List[int | datetime64]] = {
+        "bucket": [],
+        "bucket_start": [],
+        "bucket_end": [],
+        "spoiled_issues": [],
+    }
+    bucket: int = 1
+
+    itSize: int = len(it)
+
+    with Bar("Computing issue spoilage...", max=itSize // step) as bar:
+        for i in range(0, itSize, step):
+            data["bucket"].append(bucket)
+
+            bucketStart: datetime64 = (
+                (dayZero + Timedelta(days=i))
+                .to_period(freq="D")
+                .to_timestamp(how="begin")
+                .to_datetime64()
+            )
+
+            try:
+                data["spoiled_issues"].append(len(it[i : i + step]))
+                bucketEnd: datetime64 = (
+                    (dayZero + Timedelta(days=i + step - 1))
+                    .to_period(freq="D")
+                    .to_timestamp(how="end")
+                    .to_datetime64()
+                )
+            except IndexError:
+                data["spoiled_issues"].append(len(it[i:-1]))
+                bucketEnd: datetime64 = (
+                    (dayZero + Timedelta(days=itSize - 1))
+                    .to_period(freq="D")
+                    .to_timestamp(how="end")
+                    .to_datetime64()
+                )
+
+            data["bucket_start"].append(bucketStart)
+            data["bucket_end"].append(bucketEnd)
+
+            bucket += 1
+            bar.next()
+
+    return ISSUE_SPOILAGE_DF_DATAMODEL(df=DataFrame(data=data)).df
 
 
 def main(df: DataFrame) -> dict[str, DataFrame]:
@@ -37,3 +105,11 @@ def main(df: DataFrame) -> dict[str, DataFrame]:
     timeline: List[int] = list(range((dayN - dayZero).days))
 
     it: IntervalTree = buildIntervalTree(df=df, dayZero=dayZero)
+
+    df: DataFrame = computeIssueSpoilage(
+        it=it,
+        dayZero=dayZero,
+        step=7,
+    )
+
+    print(df)
