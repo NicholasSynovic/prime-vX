ef93bab7381e18bbbdb55d0faa921b9fa76a6dc7
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-04-19 12:48:35 -0500

    Reimplement SCC with the modular system

--- prime_vx/cloc/scc.py
+++ prime_vx/cloc/scc.py
@@ -1,42 +1,60 @@
 from json import dumps, loads
 from pathlib import Path
-from typing import List
+from typing import List, Tuple
 
 from pandas import DataFrame
-from pyfs import isDirectory, resolvePath, runCommand
+from pyfs import resolvePath, runCommand
 
-from prime_vx.cloc._classes._clocTool import CLOCTool_ABC
+from prime_vx.cloc import CLOC_TOOL_DATA
+from prime_vx.cloc._classes._clocTool import CLOCTool, CLOCTool_ABC
 from prime_vx.datamodels.cloc import CLOC_DF_DATAMODEL
-from prime_vx.exceptions import InvalidDirectoryPath
 
 
-class SCC(CLOCTool_ABC):
+class SCC(CLOCTool, CLOCTool_ABC):
     def __init__(self, path: Path) -> None:
         self.toolName = "scc"
+        self.command = f"{self.toolName} --by-file --min-gen --no-complexity --no-duplicates --format json {resolvePath(path=path).__str__()}"
 
-        resolvedPath: Path = resolvePath(path=path)
-
-        if isDirectory(path=resolvedPath):
-            self.path = resolvedPath
-        else:
-            raise InvalidDirectoryPath
-
-        self.command = f"{self.toolName} --by-file --min-gen --no-complexity --no-duplicates --format json {self.path.__str__()}"
+        CLOCTool(toolName=self.toolName, command=self.command, directoryPath=path)
 
     def compute(self, commitHash: str) -> DataFrame:
+        data: dict[str, List] = CLOC_TOOL_DATA
+
+        data["commit_hash"].append(commitHash)
+        data["tool"].append(self.toolName)
+
         data: dict[str, List] = {"commit_hash": [commitHash]}
 
-        output: str = runCommand(cmd=self.command).stdout.decode().strip()
-        jsonData: List = loads(s=output)
+        toolData: Tuple[dict | List, str] = self.runTool()
+        jsonDict: dict | List = toolData[0]
+        jsonStr: str = toolData[1]
 
-        data["file_count"] = [sum([len(document["Files"]) for document in jsonData])]
-        data["line_count"] = [sum([document["Lines"] for document in jsonData])]
-        data["blank_line_count"] = [sum([document["Blank"] for document in jsonData])]
-        data["comment_line_count"] = [
-            sum([document["Comment"] for document in jsonData])
-        ]
-        data["code_line_count"] = [sum([document["Code"] for document in jsonData])]
-        data["tool"] = [self.toolName]
-        data["json"] = [dumps(obj=jsonData)]
+        data["json"].append(jsonStr)
+
+        fileCount: int = sum(
+            [len(document["Files"]) for document in jsonDict],
+        )
+
+        lineCount: int = sum(
+            [document["Lines"] for document in jsonDict],
+        )
+
+        blankLineCount: int = sum(
+            [document["Blank"] for document in jsonDict],
+        )
+
+        commentLineCount: int = sum(
+            [document["Comment"] for document in jsonDict],
+        )
+
+        codeLineCount: int = sum(
+            [document["Code"] for document in jsonDict],
+        )
+
+        data["file_count"].append(fileCount)
+        data["blank_line_count"].append(blankLineCount)
+        data["comment_line_count"].append(commentLineCount)
+        data["code_line_count"].append(codeLineCount)
+        data["line_count"].append(lineCount)
 
         return CLOC_DF_DATAMODEL(df=DataFrame(data=data)).df
